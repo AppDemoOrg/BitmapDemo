87ec91e24a215ee8e0216df7ac1afa8ddbe27d7b
huangweiqi <askviky2010@gmail.com>
2018-05-20 05:18:40 +0800

    整理代码

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # BitmapDemo
+
+## 参考文献
+1、[Bitmap,jpg,png区别以及在Android上实现不载入内存压缩图片](https://blog.csdn.net/zaizai2154365/article/details/70740623)    
+2、[android 一张图片占用的内存大小 ](http://blog.sina.com.cn/s/blog_96a1468901016hcw.html)    
+3、[BitMap优化最佳实践及官方Demo“图片墙”实例详解](https://blog.csdn.net/brillianteagle/article/details/50597201)    
+
--- app/src/main/java/com/abt/bitmap/activity/MainActivity.java
+++ app/src/main/java/com/abt/bitmap/activity/MainActivity.java
@@ -3,28 +3,39 @@ package com.abt.bitmap.activity;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.abt.bitmap.R;
+import com.abt.bitmap.util.BitmapUtil;
+import com.orhanobut.logger.Logger;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static String mBasePath = "/sdcard/DCIM/Pisoft/Tmp/";
+    private String mPath = "/sdcard/DCIM/Pisoft/180308_215326.jpeg";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inJustDecodeBounds = true;//这个属性保证了仅仅解析尺寸类型，而不必加载图片
+/*        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true; // 这个属性保证了仅仅解析尺寸类型，而不必加载图片
         BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher, options);
         int imageHeight = options.outHeight;
         int imageWidth = options.outWidth;
-        String imageType = options.outMimeType;
+        String imageType = options.outMimeType;*/
+
+        getSmallBitmap(mPath);
     }
 
-    public static Bitmap decodeSampledBitmapFromResource(
-            Resources res, int resId, int reqWidth, int reqHeight) {
+    public static Bitmap decodeSampledBitmapFromResource(Resources res, int resId, int reqWidth, int reqHeight) {
         // First decode with inJustDecodeBounds=true to check dimensions
         final BitmapFactory.Options options = new BitmapFactory.Options();
         options.inJustDecodeBounds = true;
@@ -37,9 +48,74 @@ public class MainActivity extends AppCompatActivity {
         return BitmapFactory.decodeResource(res, resId, options);
     }
 
-    //Calculate Image size
-    public static int calculateInSampleSize(
-            BitmapFactory.Options options, int reqWidth, int reqHeight) {
+    public static Bitmap getSmallBitmap(String filePath) {
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(filePath, options);
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, 480, 800);
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+
+        Bitmap bm = BitmapFactory.decodeFile(filePath, options);
+        if (bm == null) {
+            return null;
+        }
+        long count = bm.getByteCount();
+        int mm = (int) count/1024/1024;
+        Logger.d("mm = "+mm);
+
+        int degree = readPictureDegree(filePath);
+        bm = rotateBitmap(bm,degree);
+        ByteArrayOutputStream baos = null;
+        try {
+            baos = new ByteArrayOutputStream();
+            bm.compress(Bitmap.CompressFormat.JPEG, 30, baos);
+        } finally {
+            try {
+                if(baos != null) baos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        BitmapUtil.saveBitmap(bm, mBasePath+"test.jpg", 100);
+        return bm;
+    }
+
+    private static int readPictureDegree(String path) {
+        int degree  = 0;
+        try {
+            ExifInterface exifInterface = new ExifInterface(path);
+            int orientation = exifInterface.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+            switch (orientation) {
+                case ExifInterface.ORIENTATION_ROTATE_90:
+                    degree = 90;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_180:
+                    degree = 180;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_270:
+                    degree = 270;
+                    break;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return degree;
+    }
+
+    private static Bitmap rotateBitmap(Bitmap bitmap, int rotate) {
+        if (bitmap == null) return null;
+        int w = bitmap.getWidth();
+        int h = bitmap.getHeight();
+
+        // Setting post rotate to 90
+        Matrix mtx = new Matrix();
+        mtx.postRotate(rotate);
+        return Bitmap.createBitmap(bitmap, 0, 0, w, h, mtx, true);
+    }
+
+    private static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
         // Raw height and width of image
         final int height = options.outHeight;
         final int width = options.outWidth;
@@ -48,12 +124,11 @@ public class MainActivity extends AppCompatActivity {
         if (height > reqHeight || width > reqWidth) {
             // Calculate ratios of height and width to requested height and width
             final int heightRatio = Math.round((float) height / (float) reqHeight);
-            final int widthRatio  = Math.round((float) width / (float) reqWidth);
-
+            final int widthRatio = Math.round((float) width / (float) reqWidth);
             // Choose the smallest ratio as inSampleSize value, this will guarantee
             // a final image with both dimensions larger than or equal to the
             // requested height and width.
-            inSampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
+            inSampleSize = heightRatio < widthRatio ? widthRatio : heightRatio;
         }
         return inSampleSize;
     }
